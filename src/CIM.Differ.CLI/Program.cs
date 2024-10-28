1d158d31fe42cdaa31ac194e0c36e21b4af46c41
runeanielsen <runenielsen@runbox.com>
2024-10-28 12:03:48 +0100

    can now take user input and process the diff

--- src/CIM.Differ.CLI/Program.cs
+++ src/CIM.Differ.CLI/Program.cs
@@ -1,6 +1,6 @@
-﻿using System.CommandLine;
-using CIM.Cson;
+﻿using CIM.Cson;
 using CIM.PhysicalNetworkModel;
+using System.CommandLine;
 
 namespace CIM.Differ.CLI;
 
@@ -8,27 +8,50 @@ internal static class Program
 {
     public static async Task<int> Main(string[] args)
     {
+        var previousStateFileOption = new Option<string>(
+            name: "--previous-state-file",
+            description: "The path to the previous state file. Example: './my-previous-state-file.jsonl'."
+        ) { IsRequired = true };
+
+        var newStateFileOption = new Option<string>(
+            name: "--new-state-file",
+            description: "The path to the new state file. Example: './my-new-state-file.jsonl'."
+        ) {  IsRequired = true };
+
+        var outputFileOption = new Option<string>(
+            name: "--output-file",
+            description: "The path and filename of the output file. Example: '/home/notation/my-new-outputfile.jsonl'."
+        ) {  IsRequired = true };
+
         var rootCommand = new RootCommand("CIM Differ CLI.");
+        rootCommand.Add(previousStateFileOption);
+        rootCommand.Add(newStateFileOption);
+        rootCommand.Add(outputFileOption);
 
-        var firstFileAbsolutePath = "";
-        var secondFileAbsolutePath = "";
-        var outputFileAbsolutePath = "";
+        rootCommand.SetHandler(async (previousStateFilePath, newStateFilePath, outputFilePath) =>
+        {
+            await ProcessDiffAsync(previousStateFilePath, newStateFilePath, outputFilePath).ConfigureAwait(false);
+        },
+        previousStateFileOption, newStateFileOption, outputFileOption);
 
+        return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
+    }
+
+    private static async Task ProcessDiffAsync(string previousStateFilePath, string newStateFilePath, string outputFilePath)
+    {
         var serializer = new CsonSerializer();
-
-        IEnumerable<IdentifiedObject> firstFileIdentifiedObjects = ReadIdentifiedObjectFile(serializer, firstFileAbsolutePath);
-        IEnumerable<IdentifiedObject> secondFileIdentifiedObjects = ReadIdentifiedObjectFile(serializer, secondFileAbsolutePath);
-
         var differ = new CimDiffer();
-        using (var destination = File.Open(outputFileAbsolutePath, FileMode.CreateNew))
+
+        var firstFileIdentifiedObjects = ReadIdentifiedObjectFile(serializer, previousStateFilePath);
+        var secondFileIdentifiedObjects = ReadIdentifiedObjectFile(serializer, newStateFilePath);
+
+        using (var destination = File.Open(outputFilePath, FileMode.CreateNew))
         {
             using (var source = serializer.SerializeObjects(differ.GetDiff(firstFileIdentifiedObjects, secondFileIdentifiedObjects)))
             {
                 await source.CopyToAsync(destination).ConfigureAwait(false);
             }
         }
-
-        return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
     }
 
     private static IEnumerable<IdentifiedObject> ReadIdentifiedObjectFile(CsonSerializer serializer, string filePath)
