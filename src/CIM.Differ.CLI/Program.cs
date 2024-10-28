b292d357ab265acc1d9f007a44eb181dc5cbb8ab
runeanielsen <runenielsen@runbox.com>
2024-10-28 11:26:00 +0100

    default setup for creating diffs

--- src/CIM.Differ.CLI/Program.cs
+++ src/CIM.Differ.CLI/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace CIM.Differ.CLI;
+﻿using System.CommandLine;
+using CIM.Cson;
+using CIM.PhysicalNetworkModel;
+
+namespace CIM.Differ.CLI;
 
 internal static class Program
 {
-    public static void Main()
+    public static async Task<int> Main(string[] args)
     {
-        Console.WriteLine("Starting CIM Differ CLI.");
+        var rootCommand = new RootCommand("CIM Differ CLI.");
+
+        var firstFileAbsolutePath = "";
+        var secondFileAbsolutePath = "";
+        var outputFileAbsolutePath = "";
+
+        var serializer = new CsonSerializer();
+
+        IEnumerable<IdentifiedObject> firstFileIdentifiedObjects = ReadIdentifiedObjectFile(serializer, firstFileAbsolutePath);
+        IEnumerable<IdentifiedObject> secondFileIdentifiedObjects = ReadIdentifiedObjectFile(serializer, secondFileAbsolutePath);
+
+        var differ = new CimDiffer();
+        using (var destination = File.Open(outputFileAbsolutePath, FileMode.CreateNew))
+        {
+            using (var source = serializer.SerializeObjects(differ.GetDiff(firstFileIdentifiedObjects, secondFileIdentifiedObjects)))
+            {
+                await source.CopyToAsync(destination).ConfigureAwait(false);
+            }
+        }
+
+        return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
+    }
+
+    private static IEnumerable<IdentifiedObject> ReadIdentifiedObjectFile(CsonSerializer serializer, string filePath)
+    {
+        using (var inputStream = File.OpenRead(filePath))
+        {
+            return serializer.DeserializeObjects(inputStream);
+        }
     }
 }
