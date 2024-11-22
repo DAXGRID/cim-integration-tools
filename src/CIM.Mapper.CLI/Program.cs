2576af9fa078e7ea9ff738979d8f1a1804d6dfd3
runeanielsen <runenielsen@runbox.com>
2024-11-22 09:15:00 +0100

    add configuration options for transformations

--- src/CIM.Mapper.CLI/Program.cs
+++ src/CIM.Mapper.CLI/Program.cs
@@ -6,6 +6,16 @@ internal static class Program
 {
     public static async Task<int> Main(string[] args)
     {
+        var transformationConfigurationFileOption = new Option<string?>(
+            name: "--transformation-configuration-file",
+            description: "The file path to the file that contains the transformation settings.."
+        ) { IsRequired = true };
+
+        var tranformationSpecificationNameOption = new Option<string?>(
+            name: "--transformation-specification-name",
+            description: "The file path to the file that contains the transformation settings.."
+        ) { IsRequired = true };
+
         var rootCommand = new RootCommand("CIM Mapper CLI.");
 
         return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
