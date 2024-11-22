c60e795010f910bf3236817c0069ed1fecb6a3b0
runeanielsen <runenielsen@runbox.com>
2024-11-22 13:50:12 +0100

    can now execute the CIM.Mapper.CLI

--- src/CIM.Mapper.CLI/Program.cs
+++ src/CIM.Mapper.CLI/Program.cs
@@ -1,4 +1,10 @@
-﻿using System.CommandLine;
+﻿using DAX.IO;
+using DAX.IO.CIM;
+using DAX.IO.CIM.DataModel;
+using DAX.IO.CIM.Processing;
+using DAX.IO.Transformers;
+using DAX.IO.Writers;
+using System.CommandLine;
 
 namespace CIM.Mapper.CLI;
 
@@ -24,23 +30,123 @@ internal static class Program
         )
         { IsRequired = false };
 
+        var debugOption = new Option<bool>(
+            name: "--debug",
+            description: "Run the program in debug mode."
+        )
+        { IsRequired = false };
+
+        var serializerOption = new Option<string?>(
+            name: "--serializer",
+            description: "The name of the serializer."
+        )
+        { IsRequired = false };
+
+        var outputFilePathOption = new Option<string?>(
+            name: "--output",
+            description: "The output file path."
+        )
+        { IsRequired = false };
+
         var rootCommand = new RootCommand("CIM Mapper CLI.");
         rootCommand.Add(transformationConfigurationFileOption);
         rootCommand.Add(tranformationSpecificationNameOption);
         rootCommand.Add(forceOption);
+        rootCommand.Add(debugOption);
+        rootCommand.Add(serializerOption);
+        rootCommand.Add(outputFilePathOption);
 
         rootCommand.SetHandler(
-            (transformationConfigurationFile, tranformationSpecificationName, force) =>
+            (transformationConfigurationFile, tranformationSpecificationName, force, debugMode, serializerName, outputFilePath) =>
             {
-                Console.WriteLine(transformationConfigurationFile);
-                Console.WriteLine(tranformationSpecificationName);
-                Console.WriteLine(force);
+                // Check if transformation configuration file exists
+                if (!File.Exists(transformationConfigurationFile))
+                {
+                    throw new ArgumentException(
+                        $"Cannot find transformation configuration file: '{transformationConfigurationFile}'.");
+                }
+
+                var config = new TransformationConfig().LoadFromFile(transformationConfigurationFile);
+                var transSpecNameSplit = tranformationSpecificationName.Split(',');
+
+                foreach (string transSpecName in transSpecNameSplit)
+                {
+                    if (config.GetTransformationSpecification(transSpecName.Trim()) == null)
+                    {
+                        throw new ArgumentException(
+                            $"Cannot find transformation specification name: '{transSpecName.Trim()}' in transformation specification configuration file: '{transformationConfigurationFile}'.");
+                    }
+                }
+
+                foreach (string transSpecName in transSpecNameSplit)
+                {
+                    var transSpec = config.GetTransformationSpecification(transSpecName.Trim());
+                    var transformer = config.InitializeDataTransformer(transSpecName);
+                    var guide = transformer.Simulate();
+
+                    if (debugMode)
+                    {
+                        Console.WriteLine(guide.TextReport());
+                    }
+
+                    if (guide.ProblemsFound > 0)
+                    {
+                        Console.Error.WriteLine(guide.TextReport());
+
+                        if (!force)
+                            Console.Error.WriteLine("Error found in mapping (se log). Transformation {transSpecName} is terminated! Please correct mapping and try again.");
+                        else
+                        {
+                            Console.Error.WriteLine("Error found in mapping (se log). But will attempt to transfer data because of force parameter.");
+                            transformer.TransferData();
+                            CheckForCIMProcessing(serializerName, outputFilePath, config, transformer);
+                        }
+                    }
+                    else
+                    {
+                        transformer.TransferData();
+                        CheckForCIMProcessing(serializerName, outputFilePath, config, transformer);
+                    }
+                }
             },
             transformationConfigurationFileOption,
             tranformationSpecificationNameOption,
-            forceOption
+            forceOption,
+            debugOption,
+            serializerOption,
+            outputFilePathOption
         );
 
         return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
+    }
+
+    private static void CheckForCIMProcessing(
+        string? serializerName,
+        string? outputFileName,
+        TransformationConfig config,
+        DataTransformer transformer)
+    {
+        if (config.TransformationSpecifications != null && transformer != null)
+        {
+            var writer = transformer.GetFirstDataWriter() as CIMGraphWriter;
+
+            if (writer != null)
+            {
+                CIMGraph graph = writer.GetCIMGraph();
+
+                if (serializerName is not null && outputFileName is not null)
+                {
+                    Console.WriteLine($"Running '{serializerName}' serializer - writing to: '{outputFileName}'.");
+
+                    var serializer = config.InitializeSerializer(serializerName);
+
+                    var compressedData = ((IDAXSerializeable)serializer).Serialize(CIMMetaDataManager.Repository, graph.CIMObjects, graph);
+
+                    var fileStream = new System.IO.FileStream(outputFileName, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+                    fileStream.Write(compressedData, 0, compressedData.Length);
+                    fileStream.Close();
+                }
+            }
+        }
     }
 }
