cc087a196cce047d30b1a42d986afbb728a53a4d
runeanielsen <runenielsen@runbox.com>
2024-11-22 10:59:11 +0100

    can now handle the default command for mapping

--- src/CIM.Mapper.CLI/Program.cs
+++ src/CIM.Mapper.CLI/Program.cs
@@ -6,17 +6,28 @@ internal static class Program
 {
     public static async Task<int> Main(string[] args)
     {
-        var transformationConfigurationFileOption = new Option<string?>(
+        var transformationConfigurationFileOption = new Option<string>(
             name: "--transformation-configuration-file",
             description: "The file path to the file that contains the transformation settings.."
-        ) { IsRequired = true };
+        )
+        { IsRequired = true };
 
-        var tranformationSpecificationNameOption = new Option<string?>(
+        var tranformationSpecificationNameOption = new Option<string>(
             name: "--transformation-specification-name",
             description: "The file path to the file that contains the transformation settings.."
-        ) { IsRequired = true };
+        )
+        { IsRequired = true };
 
         var rootCommand = new RootCommand("CIM Mapper CLI.");
+        rootCommand.Add(transformationConfigurationFileOption);
+        rootCommand.Add(tranformationSpecificationNameOption);
+
+        rootCommand.SetHandler(
+            (transformationConfigurationFile, tranformationSpecificationName) =>
+            {
+                Console.WriteLine(transformationConfigurationFile);
+                Console.WriteLine(tranformationSpecificationName);
+            }, transformationConfigurationFileOption, tranformationSpecificationNameOption);
 
         return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
     }
