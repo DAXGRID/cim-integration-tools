e9a520f0c013739606f38c2a9a4d34a44cb9f41b
runeanielsen <runenielsen@runbox.com>
2024-11-27 11:57:08 +0100

    add better documentation for the CIM.Mapper.CLI

--- README.md
+++ README.md
@@ -9,3 +9,11 @@ Example of how to use the CIM.Differ.CLI.
 ```sh
 ./CIM.Differ.CLI --previous-state-file='./my-previous-state-file.jsonl' --new-state-file='./my-new-state-file.jsonl' --output-file=''./my-new-outputfile.jsonl''
 ```
+
+## CIM Mapper
+
+Example of how to use the CIM.Mapper.CLI.
+
+```sh
+./CIM.Mapper.CLI --transformation-configuration-file="./TransformationConfig.xml" --new-state-file='specification_one,specification_two,specification_three'
+```
--- src/CIM.Mapper.CLI/Program.cs
+++ src/CIM.Mapper.CLI/Program.cs
@@ -14,13 +14,13 @@ internal static class Program
     {
         var transformationConfigurationFileOption = new Option<string>(
             name: "--transformation-configuration-file",
-            description: "The file path to the file that contains the transformation settings."
+            description: "The file path to the file that contains the transformation settings. Example: './TransformationConfig.xml'."
         )
         { IsRequired = true };
 
         var tranformationSpecificationNameOption = new Option<string>(
             name: "--transformation-specification-name",
-            description: "The specification names."
+            description: "The specification names. Example: 'specification_one,specification_two,specification_three'."
         )
         { IsRequired = true };
 
