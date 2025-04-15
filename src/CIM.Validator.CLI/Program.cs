1d4f36c7a5970cbbca24af8146d0baff3b7c517c
runeanielsen <runenielsen@runbox.com>
2025-04-15 15:45:54 +0200

    add simple data model for validation errors

--- src/CIM.Validator.CLI/Program.cs
+++ src/CIM.Validator.CLI/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using System.CommandLine;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace CIM.Validator.CLI;
 
@@ -13,6 +15,54 @@ internal static class Program
 
         logger.LogInformation("Starting CIM Validator.");
 
+        var validationErrors = new List<ValidationError>
+        {
+            new ValidationError
+            {
+                Mrid = Guid.NewGuid(),
+                TypeName = "PowerTransformerEnd",
+                Code = "NO_RATED_S",
+                Description = "ratedS value not set",
+                Severity = Severity.Warning
+            },
+            new ValidationError
+            {
+                Mrid = Guid.NewGuid(),
+                TypeName = "PowerTransformerEnd",
+                Code = "NO_RATED_S",
+                Description = "ratedS value not set",
+                Severity = Severity.Warning
+            },
+        };
+
+        using (StreamWriter sw = new StreamWriter("./warnings.jsonl"))
+        {
+            foreach (var validationError in validationErrors)
+            {
+                await sw.WriteLineAsync(JsonSerializer.Serialize(validationError)).ConfigureAwait(false);
+            }
+        }
+
         return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
     }
+}
+
+internal enum Severity
+{
+    Warning,
+    Error
+}
+
+internal sealed record ValidationError
+{
+    [JsonPropertyName("$type")]
+    public string TypeName { get; private init; } = "ValidationError";
+
+    public required Guid Mrid { get; init; }
+
+    public required Severity Severity { get; init; }
+
+    public required string Code { get; init; }
+
+    public required string Description { get; init; }
 }
