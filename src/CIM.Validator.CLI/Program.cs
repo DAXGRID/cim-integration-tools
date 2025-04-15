692613d74b5d7e5c4fb064d46069c7ac641bf3e4
runeanielsen <runenielsen@runbox.com>
2025-04-15 13:55:44 +0200

    add project template for CIM validator CLI

---
+++ src/CIM.Validator.CLI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using System.CommandLine;
+
+namespace CIM.Validator.CLI;
+
+internal static class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        var rootCommand = new RootCommand("CIM Validator CLI.");
+
+        var logger = LoggerFactory.Create(nameof(CIM.Validator.CLI));
+
+        logger.LogInformation("Starting CIM Validator.");
+
+        return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
+    }
+}
