90608d5a2e5ac8a54a53f056773472674b36762d
runeanielsen <runenielsen@runbox.com>
2025-04-17 13:15:34 +0200

    now validates that terminal points to a conducting equipment that exists

--- src/CIM.Validator.CLI/TerminalValidation.cs
+++ src/CIM.Validator.CLI/TerminalValidation.cs
@@ -1,10 +1,11 @@
+using System.Collections.Frozen;
 using CIM.PhysicalNetworkModel;
 
 namespace CIM.Validator.CLI;
 
 internal static class TerminalValidation
 {
-    public static ValidationError? ConductingEquipmentReference(Terminal t)
+    public static ValidationError? ConductingEquipmentReferenceId(Terminal t)
     {
         if (t.ConductingEquipment?.@ref is null)
         {
@@ -14,6 +15,23 @@ internal static class TerminalValidation
                 TypeName = t.GetType().Name,
                 Code = "TERMINAL_IS_MISSING_MANDATORY_CONDUCTING_EQUIPMENT_REFERENCE",
                 Description = "The terminal is missing a reference to a conducting equipment.",
+                Severity = Severity.Warning
+            };
+        }
+
+        return null;
+    }
+
+    public static ValidationError? ConductingEquipmentReferenceExist(Terminal t, FrozenSet<Guid> conductingEquipmentMrIds)
+    {
+        if (t.ConductingEquipment?.@ref is not null && !conductingEquipmentMrIds.Contains(Guid.Parse(t.ConductingEquipment.@ref)))
+        {
+            return new ValidationError
+            {
+                Mrid = Guid.Parse(t.mRID),
+                TypeName = t.GetType().Name,
+                Code = "TERMINAL_CONDUCTING_EQUIPMENT_REFERENCE_DOES_NOT_EXIST",
+                Description = "The terminal reference a conducting equipment that does not exist.",
                 Severity = Severity.Warning
             };
         }
