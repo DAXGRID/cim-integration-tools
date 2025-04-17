285402d250fc7de28c95f92883c40c241d634465
runeanielsen <runenielsen@runbox.com>
2025-04-17 10:17:42 +0200

    optimize solution

--- src/CIM.Validator.CLI/Program.cs
+++ src/CIM.Validator.CLI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using CIM.Cson;
 using CIM.PhysicalNetworkModel;
 using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+using System.Collections.Frozen;
 using System.CommandLine;
 using System.Text.Json;
 
@@ -19,34 +21,17 @@ internal static class Program
 
         logger.LogInformation("Starting CIM Validator.");
 
-        var conductingEquipments = new List<ConductingEquipment>();
-        var terminals = new List<Terminal>();
+        var validationErrors = new ConcurrentBag<ValidationError>();
 
-        var validationErrors = new List<ValidationError>();
-
-        var serializer = new CsonSerializer();
-        await foreach (var line in File.ReadLinesAsync(inputFile).ConfigureAwait(false))
-        {
-            var identifiedObject = serializer.DeserializeObject(line);
-
-            if (identifiedObject is ConductingEquipment)
-            {
-                conductingEquipments.Add((ConductingEquipment)identifiedObject);
-            }
-
-            if (identifiedObject is Terminal)
-            {
-                terminals.Add((Terminal)identifiedObject);
-            }
-        }
+        var (conductingEquipments, terminals) = await LoadCimAsync(inputFile).ConfigureAwait(false);
 
         var terminalsByConductingEquipment = terminals
             .GroupBy(x => x.ConductingEquipment.@ref)
-            .ToDictionary(x => x.Key, x => x.ToList());
+            .ToFrozenDictionary(x => x.Key, x => x.ToArray());
 
-        foreach (var conductingEquipment in conductingEquipments)
+        Parallel.ForEach(conductingEquipments, (conductingEquipment) =>
         {
-            var conductingEquipmentTerminals = terminalsByConductingEquipment.GetValueOrDefault(conductingEquipment.mRID) ?? new List<Terminal>();
+            var conductingEquipmentTerminals = terminalsByConductingEquipment.GetValueOrDefault(conductingEquipment.mRID) ?? Array.Empty<Terminal>();
 
             var validationsConductingEquipment = new List<Func<ValidationError?>>
             {
@@ -64,8 +49,15 @@ internal static class Program
                     validationErrors.Add(validationError);
                 }
             }
-        }
+        });
 
+        await WriteValidationErrors(outputFile, validationErrors).ConfigureAwait(false);
+
+        return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
+    }
+
+    private static async Task WriteValidationErrors(string outputFile, ConcurrentBag<ValidationError> validationErrors)
+    {
         using (var sw = new StreamWriter(outputFile))
         {
             foreach (var validationError in validationErrors)
@@ -73,7 +65,29 @@ internal static class Program
                 await sw.WriteLineAsync(JsonSerializer.Serialize(validationError)).ConfigureAwait(false);
             }
         }
+    }
 
-        return await rootCommand.InvokeAsync(args).ConfigureAwait(false);
+    private static async Task<(FrozenSet<ConductingEquipment>, FrozenSet<Terminal>)> LoadCimAsync(string inputFile)
+    {
+        var conductingEquipments = new List<ConductingEquipment>();
+        var terminals = new List<Terminal>();
+
+        var serializer = new CsonSerializer();
+        await foreach (var line in File.ReadLinesAsync(inputFile).ConfigureAwait(false))
+        {
+            var identifiedObject = serializer.DeserializeObject(line);
+
+            if (identifiedObject is ConductingEquipment)
+            {
+                conductingEquipments.Add((ConductingEquipment)identifiedObject);
+            }
+
+            if (identifiedObject is Terminal)
+            {
+                terminals.Add((Terminal)identifiedObject);
+            }
+        }
+
+        return (conductingEquipments.ToFrozenSet(), terminals.ToFrozenSet());
     }
 }
