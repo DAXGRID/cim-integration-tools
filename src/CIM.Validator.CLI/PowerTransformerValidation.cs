142b69cdfa7770dd4245aa2ce8d6372f3a70ca5e
runeanielsen <runenielsen@runbox.com>
2025-04-17 14:35:08 +0200

    add missing power transformer validation

---
+++ src/CIM.Validator.CLI/PowerTransformerValidation.cs
@@ -0,0 +1,23 @@
+using CIM.PhysicalNetworkModel;
+
+namespace CIM.Validator.CLI;
+
+internal static class PowerTransformerValidation
+{
+    public static ValidationError? PowerTransformerEndPerTerminal(PowerTransformer powerTransformer, IReadOnlyList<Terminal> terminals, IReadOnlyList<PowerTransformerEnd> powerTransformerEnds)
+    {
+        if (!powerTransformerEnds.All(pe => terminals.Any(t => pe.Terminal.@ref == t.mRID)))
+        {
+            return new ValidationError
+            {
+                Mrid = Guid.Parse(powerTransformer.mRID),
+                TypeName = powerTransformer.GetType().Name,
+                Code = "ONE_POWER_TRANSFORMER_END_PER_TERMINAL",
+                Description = "One power transformer end per terminal is required.",
+                Severity = Severity.Warning
+            };
+        }
+
+        return null;
+    }
+}
