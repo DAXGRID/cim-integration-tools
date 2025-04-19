dfafec186b02a97796b7d1cda81b74e281052a21
runeanielsen <runenielsen@runbox.com>
2025-04-19 13:50:03 +0200

    can now validate PSR Type on substation

---
+++ src/CIM.Validator.CLI/SubstationValidation.cs
@@ -0,0 +1,22 @@
+using CIM.PhysicalNetworkModel;
+
+namespace CIM.Validator.CLI;
+
+internal static class SubstationValidation
+{
+    public static ValidationError? PsrType(Substation s)
+    {
+        return s.PSRType switch
+        {
+            "PrimarySubstation" or "SecondarySubstation" or "Tower" or "CableBox" => null,
+            _ => new ValidationError
+            {
+                Mrid = Guid.Parse(s.mRID),
+                TypeName = s.GetType().Name,
+                Code = "INCORRECT_PSR_TYPE_FOR_SUBSTATION",
+                Description = $"Incorrect PSR type for substation, should be PrimarySubstation, SecondarySubstation, Tower or CableBox. The supplied PSR type is: '{s.PSRType}'.",
+                Severity = Severity.Warning
+            }
+        };
+    }
+}
