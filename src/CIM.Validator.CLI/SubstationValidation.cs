03ab5f66c69e05b0e3b6e22ea9f5bec2c3ffe542
runeanielsen <runenielsen@runbox.com>
2025-04-19 13:52:16 +0200

    switch should be of type error

--- src/CIM.Validator.CLI/SubstationValidation.cs
+++ src/CIM.Validator.CLI/SubstationValidation.cs
@@ -15,7 +15,7 @@ internal static class SubstationValidation
                 TypeName = s.GetType().Name,
                 Code = "INCORRECT_PSR_TYPE_FOR_SUBSTATION",
                 Description = $"Incorrect PSR type for substation, should be PrimarySubstation, SecondarySubstation, Tower or CableBox. The supplied PSR type is: '{s.PSRType}'.",
-                Severity = Severity.Warning
+                Severity = Severity.Error
             }
         };
     }
