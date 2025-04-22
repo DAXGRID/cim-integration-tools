d3841c52123faa3cf740121a6b7bb85c0110bab6
runeanielsen <runenielsen@runbox.com>
2025-04-22 10:29:47 +0200

    T-Junction is now a valid PSR type

--- src/CIM.Validator.CLI/SubstationValidation.cs
+++ src/CIM.Validator.CLI/SubstationValidation.cs
@@ -49,13 +49,13 @@ internal static class SubstationValidation
     {
         return s.PSRType switch
         {
-            "PrimarySubstation" or "SecondarySubstation" or "Tower" or "CableBox" => null,
+            "PrimarySubstation" or "SecondarySubstation" or "Tower" or "CableBox" or "T-Junction" => null,
             _ => new ValidationError
             {
                 Mrid = Guid.Parse(s.mRID),
                 TypeName = s.GetType().Name,
                 Code = "INCORRECT_PSR_TYPE_FOR_SUBSTATION",
-                Description = $"Incorrect PSR type for substation, should be PrimarySubstation, SecondarySubstation, Tower or CableBox. The supplied PSR type is: '{s.PSRType}'.",
+                Description = $"Incorrect PSR type for substation, should be PrimarySubstation, SecondarySubstation, Tower, T-Junction or CableBox. The supplied PSR type is: '{s.PSRType}'.",
                 Severity = Severity.Error
             }
         };
