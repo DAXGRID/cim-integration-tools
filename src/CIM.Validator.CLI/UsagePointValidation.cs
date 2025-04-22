2dab5a332a0080c56e30b5d982dc24057476e3a8
runeanielsen <runenielsen@runbox.com>
2025-04-22 10:27:59 +0200

    remove ending space in error code

--- src/CIM.Validator.CLI/UsagePointValidation.cs
+++ src/CIM.Validator.CLI/UsagePointValidation.cs
@@ -12,7 +12,7 @@ internal static class UsagePointValidation
             {
                 Mrid = Guid.Parse(u.mRID),
                 TypeName = u.GetType().Name,
-                Code = "USAGE_POINT_EQUIPMENT_REFERENCE_REQUIRED ",
+                Code = "USAGE_POINT_EQUIPMENT_REFERENCE_REQUIRED",
                 Description = "Usage point always requires an equipment reference.",
                 Severity = Severity.Error
             };
