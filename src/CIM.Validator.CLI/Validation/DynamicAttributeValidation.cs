9123e01e4e9734c42be2564457a70a6994c1eff5
runeanielsen <runenielsen@runbox.com>
2025-07-24 10:02:25 +0200

    optimized dynamic attribute validation

--- src/CIM.Validator.CLI/Validation/DynamicAttributeValidation.cs
+++ src/CIM.Validator.CLI/Validation/DynamicAttributeValidation.cs
@@ -21,17 +21,13 @@ internal static class DynamicAttributeValidation
             "mainAddress"
         };
 
-        foreach (var propertyInfo in objectType.GetProperties())
+        foreach (var propertyInfo in objectType.GetProperties().Where(x => !ignorePropertiesLookup.Contains(x.Name)))
         {
-            var propertyValue = propertyInfo.GetValue(x);
             var propertyName = propertyInfo.Name;
-            if (ignorePropertiesLookup.Contains(propertyName))
-            {
-                continue;
-            }
+            var propertyValue = propertyInfo.GetValue(x);
 
-            // Primitive type
-            if (propertyInfo.PropertyType.IsPrimitive && propertyValue is null || (propertyValue is string && string.IsNullOrWhiteSpace(((string)propertyValue))))
+            if (propertyInfo.PropertyType.IsPrimitive && propertyValue is null || (propertyValue is string && string.IsNullOrWhiteSpace(((string)propertyValue))) ||
+                !propertyInfo.PropertyType.GetProperties().Any(x => x.Name == "ref") && propertyValue is null)
             {
                 validationErrors.Add(new ValidationError
                 {
@@ -40,17 +36,6 @@ internal static class DynamicAttributeValidation
                     Description = $"{propertyName} value is not set.",
                     IdentifiedObjectId = objectType.GetProperty("mRID").GetValue(x)?.ToString() ?? throw new InvalidOperationException("The mRID could not be found on the object, something is wrong, this should never happen."),
                     IdentifiedObjectClass = objectType.Name.ToUpperInvariant()
-                });
-            }
-            else if (!propertyInfo.PropertyType.GetProperties().Any(x => x.Name == "ref") && propertyValue is null)
-            {
-                validationErrors.Add(new ValidationError
-                {
-                    Code = $"{propertyName.ToSnakeCaseUpper()}_IS_NULL",
-                    Severity = Severity.Warning,
-                    Description = $"{propertyName} value is not set.",
-                    IdentifiedObjectId = objectType.GetProperty("mRID").GetValue(x)?.ToString() ?? throw new InvalidOperationException("The mRID could not be found on the object, something is wrong, this should never happen."),
-                    IdentifiedObjectClass = objectType.Name
                 });
             }
         }
